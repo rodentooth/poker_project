1528a64b57bc12b480cd259d584b150353abf76d
Emanuel Graf <emanuel.smile@hotmail.com>
2018-03-10 18:51:18 +0100

    rotation but failing

--- src/main/Controller/poker_game_1_controller.java
+++ src/main/Controller/poker_game_1_controller.java
@@ -1,11 +1,13 @@
 package main.Controller;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import main.Model.Game.Poker_5_Stud;
 import main.Model.Ranking.Hand_Ranks;
 import main.Model.Ranking.Ranking;
@@ -15,15 +17,19 @@ import main.View.poker_game_1;
 
 import java.util.ArrayList;
 
+import static javafx.scene.transform.Rotate.Y_AXIS;
+
 public class poker_game_1_controller {
 
     Poker_5_Stud model;
     poker_game_1 view;
     VBox box1;
     ArrayList<ArrayList<Card>> all_hands = null;
+    Boolean once = false;
 
 
     public poker_game_1_controller(Poker_5_Stud model, poker_game_1 view, ArrayList<String> savedNames) {
+
 
         this.view = view;
         this.model = model;
@@ -68,97 +74,42 @@ public class poker_game_1_controller {
                     }
                 }
 
+                dealout(savedNames);
 
 
 
             }
         });
 
+        Timeline time = new Timeline();
+        time.setCycleCount(1);
+        time.setAutoReverse(false);
 
         view.players.setMaxWidth(Double.MAX_VALUE);
         view.players.setMaxHeight(Double.MAX_VALUE);
 
         view.deal_btn.setOnAction((event) -> {
 
-            System.out.println("Players in our game: " + savedNames.size());
+            if (once) {
 
+                for (int i = 1; i < (all_hands.size() + 1); i++) {
 
-            if (view.players.getChildren() != null)
-                view.players.getChildren().clear();
+                    HBox player_pane = (((HBox) (get_Specific_player_pane(i)).getChildren().get(1)));
 
+                    for (int j = 0; j < (5); j++) {
+                        Pane p = (Pane) player_pane.getChildren().get(j);
 
+                        RotateTransition rt = new RotateTransition(Duration.millis(3000), p);
+                        rt.setByAngle(-180);
+                        rt.setAxis(Y_AXIS);
+                        rt.setCycleCount(1);
 
-            all_hands = model.getHands(savedNames.size());
+                        rt.play();
 
-
-
-            for (int i = 0; i < all_hands.size(); i++) {
-                Player_Pane_Appearance Player_Pane_Apperance = new Player_Pane_Appearance();
-
-                HBox section;
-                if (((all_hands.size() <= 5) && (i % 2 == 0)) || ((all_hands.size() > 5) && (i % 4 == 0))) {
-                    section = new HBox();
-                    // section.setStyle("-fx-background-color: #00" + i + "0ff");
-                    section.setPrefSize(1000, 300);
-                    view.players.getChildren().add(section);
-
-
-
-                } else {
-
-
-                    section = (HBox) view.players.getChildren().get(view.players.getChildren().size() - 1);
-
-
+                    }
                 }
-
-
-                section.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-
-                AnchorPane.setTopAnchor(view.players, 10.0);
-                AnchorPane.setBottomAnchor(view.players, 10.0);
-                AnchorPane.setRightAnchor(view.players, 10.0);
-                AnchorPane.setTopAnchor(section, 10.0);
-
-                section.setSpacing(10);
-                box1 = Player_Pane_Apperance.Create_Plpa(all_hands.get(i));
-
-                box1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-
-
-                box1.setStyle("-fx-background-color: #003700");
-                box1.setBorder(new Border(new BorderStroke(Color.BLACK,
-                        BorderStrokeStyle.SOLID, new CornerRadii(10), new BorderWidths(20))));
-
-
-                Label lbl1 = (Label) (((HBox) box1.getChildren().get(0)).getChildren().get(0));
-                lbl1.setText(savedNames.get(i));
-
-                Label lbl2 = (Label) (((HBox) box1.getChildren().get(2)).getChildren().get(0));
-
-                Ranking r = new Ranking();
-
-                lbl2.setText("It is a " + (String.valueOf(Hand_Ranks.values()[Math.abs(r.rank_hand(all_hands.get(i)) - 10)])).replace("_", " "));
-
-                //Label lbl3 = (Label) (((HBox) box1.getChildren().get(3)).getChildren().get(0));
-                //lbl3.setText("You are a ...");
-
-
-                section.getChildren().add(box1);
-
-
-                //PlayerPane.setPlayer(model.getPlayer(i));
-
-
             }
-
-
-            Stage stageTheEventSourceNodeBelongs = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            // OR
-            Stage stageTheLabelBelongs = (Stage) view.deck_txt.getScene().getWindow();
-
-            stageTheLabelBelongs.sizeToScene();
-
+            once = true;
         });
 
 
@@ -172,7 +123,85 @@ public class poker_game_1_controller {
         });
 */
 
+        dealout(savedNames);
 
+
+    }
+
+    private void dealout(ArrayList<String> savedNames) {
+        System.out.println("Players in our game: " + savedNames.size());
+
+
+        if (view.players.getChildren() != null)
+            view.players.getChildren().clear();
+
+
+        all_hands = model.getHands(savedNames.size());
+
+
+        for (int i = 0; i < all_hands.size(); i++) {
+            Player_Pane_Appearance Player_Pane_Apperance = new Player_Pane_Appearance();
+
+            HBox section;
+            if (((all_hands.size() <= 5) && (i % 2 == 0)) || ((all_hands.size() > 5) && (i % 4 == 0))) {
+                section = new HBox();
+                // section.setStyle("-fx-background-color: #00" + i + "0ff");
+                section.setPrefSize(1000, 300);
+                view.players.getChildren().add(section);
+
+
+            } else {
+
+
+                section = (HBox) view.players.getChildren().get(view.players.getChildren().size() - 1);
+
+
+            }
+
+
+            section.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+            AnchorPane.setTopAnchor(view.players, 10.0);
+            AnchorPane.setBottomAnchor(view.players, 10.0);
+            AnchorPane.setRightAnchor(view.players, 10.0);
+            AnchorPane.setTopAnchor(section, 10.0);
+
+            section.setSpacing(10);
+            box1 = Player_Pane_Apperance.Create_Plpa(all_hands.get(i));
+
+            box1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+
+            box1.setStyle("-fx-background-color: #003700");
+            box1.setBorder(new Border(new BorderStroke(Color.BLACK,
+                    BorderStrokeStyle.SOLID, new CornerRadii(10), new BorderWidths(20))));
+
+
+            Label lbl1 = (Label) (((HBox) box1.getChildren().get(0)).getChildren().get(0));
+            lbl1.setText(savedNames.get(i));
+
+            Label lbl2 = (Label) (((HBox) box1.getChildren().get(2)).getChildren().get(0));
+
+            Ranking r = new Ranking();
+
+            lbl2.setText("It is a " + (String.valueOf(Hand_Ranks.values()[Math.abs(r.rank_hand(all_hands.get(i)) - 10)])).replace("_", " "));
+
+            //Label lbl3 = (Label) (((HBox) box1.getChildren().get(3)).getChildren().get(0));
+            //lbl3.setText("You are a ...");
+
+
+            section.getChildren().add(box1);
+
+
+            //PlayerPane.setPlayer(model.getPlayer(i));
+
+
+        }
+
+
+        Stage stageTheLabelBelongs = (Stage) view.deck_txt.getScene().getWindow();
+
+        stageTheLabelBelongs.sizeToScene();
     }
 
 
--- src/main/View/Object_Appearance/Card_Apperance.java
+++ src/main/View/Object_Appearance/Card_Apperance.java
@@ -1,9 +1,12 @@
 package main.View.Object_Appearance;
 
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.transform.Rotate;
 import main.Model.Stack.Card;
 
 public class Card_Apperance {
@@ -11,7 +14,10 @@ public class Card_Apperance {
     public Pane Card_Apperance(Card c) {
 
 
-        Pane inside_card2 = new Pane();
+        Pane container = new Pane();
+        StackPane inside_card2 = new StackPane();
+        inside_card2.setAlignment(Pos.CENTER);
+
 
         //inside_card.setPadding(new Insets(15, 12, 15, 12));
 
@@ -30,235 +36,29 @@ public class Card_Apperance {
 
         imageView2.setImage(shortImageUrl);
 
-        /*
-        String card = new String(c.getSuit().toString() + c.getRank().toString());
-        Image image = new Image("main.res.Card_Images/Spades/Spades_Ace.png");
-        Image image1 = new Image("main.res.Card_Images/Spades/Spades_King.png");
-        Image image2 = new Image("main.res.Card_Images/Spades/Spades_Queen.png");
-        Image image3 = new Image("main.res.Card_Images/Spades/Spades_Jack.png");
-        Image image4 = new Image("main.res.Card_Images/Spades/Spades_Ten.png");
-        Image image5 = new Image("main.res.Card_Images/Spades/Spades_Nine.png");
-        Image image6 = new Image("main.res.Card_Images/Spades/Spades_Eight.png");
-        Image image7 = new Image("main.res.Card_Images/Spades/Spades_Seven.png");
-        Image image8 = new Image("main.res.Card_Images/Spades/Spades_Six.png");
-        Image image9 = new Image("main.res.Card_Images/Spades/Spades_Five.png");
-        Image image10 = new Image("main.res.Card_Images/Spades/Spades_Four.png");
-        Image image11 = new Image("main.res.Card_Images/Spades/Spades_Three.png");
-        Image image12 = new Image("main.res.Card_Images/Spades/Spades_Two.png");
-        Image image13 = new Image("main.res.Card_Images/Hearts/Hearts_Ace.png");
-        Image image14 = new Image("main.res.Card_Images/Hearts/Hearts_Eight.png");
-        Image image15 = new Image("main.res.Card_Images/Hearts/Hearts_Five.png");
-        Image image16 = new Image("main.res.Card_Images/Hearts/Hearts_Four.png");
-        Image image17 = new Image("main.res.Card_Images/Hearts/Hearts_Jack.png");
-        Image image18 = new Image("main.res.Card_Images/Hearts/Hearts_King.png");
-        Image image19 = new Image("main.res.Card_Images/Hearts/Hearts_Nine.png");
-        Image image20 = new Image("main.res.Card_Images/Hearts/Hearts_Queen.png");
-        Image image21 = new Image("main.res.Card_Images/Hearts/Hearts_Seven.png");
-        Image image22 = new Image("main.res.Card_Images/Hearts/Hearts_Six.png");
-        Image image23 = new Image("main.res.Card_Images/Hearts/Hearts_Ten.png");
-        Image image24 = new Image("main.res.Card_Images/Hearts/Hearts_Three.png");
-        Image image25 = new Image("main.res.Card_Images/Hearts/Hearts_Two.png");
-        Image image26 = new Image("main.res.Card_Images/Diamonds/Diamonds_Ace.png");
-        Image image27 = new Image("main.res.Card_Images/Diamonds/Diamonds_Eight.png");
-        Image image28 = new Image("main.res.Card_Images/Diamonds/Diamonds_Five.png");
-        Image image29 = new Image("main.res.Card_Images/Diamonds/Diamonds_Four.png");
-        Image image30 = new Image("main.res.Card_Images/Diamonds/Diamonds_Jack.png");
-        Image image31 = new Image("main.res.Card_Images/Diamonds/Diamonds_King.png");
-        Image image32 = new Image("main.res.Card_Images/Diamonds/Diamonds_Nine.png");
-        Image image33 = new Image("main.res.Card_Images/Diamonds/Diamonds_Queen.png");
-        Image image34 = new Image("main.res.Card_Images/Diamonds/Diamonds_Seven.png");
-        Image image35 = new Image("main.res.Card_Images/Diamonds/Diamonds_Six.png");
-        Image image36 = new Image("main.res.Card_Images/Diamonds/Diamonds_Ten.png");
-        Image image37 = new Image("main.res.Card_Images/Diamonds/Diamonds_Three.png");
-        Image image38 = new Image("main.res.Card_Images/Diamonds/Diamonds_Two.png");
-        Image image39 = new Image("main.res.Card_Images/Clubs/Clubs_Ace.png");
-        Image image40 = new Image("main.res.Card_Images/Clubs/Clubs_Eight.png");
-        Image image41 = new Image("main.res.Card_Images/Clubs/Clubs_Five.png");
-        Image image42 = new Image("main.res.Card_Images/Clubs/Clubs_Four.png");
-        Image image43 = new Image("main.res.Card_Images/Clubs/Clubs_Jack.png");
-        Image image44 = new Image("main.res.Card_Images/Clubs/Clubs_King.png");
-        Image image45 = new Image("main.res.Card_Images/Clubs/Clubs_Nine.png");
-        Image image46 = new Image("main.res.Card_Images/Clubs/Clubs_Queen.png");
-        Image image47 = new Image("main.res.Card_Images/Clubs/Clubs_Seven.png");
-        Image image48 = new Image("main.res.Card_Images/Clubs/Clubs_Six.png");
-        Image image49 = new Image("main.res.Card_Images/Clubs/Clubs_Ten.png");
-        Image image50 = new Image("main.res.Card_Images/Clubs/Clubs_Three.png");
-        Image image51 = new Image("main.res.Card_Images/Clubs/Clubs_Two.png");
+        imageView2.getTransforms().add(new Rotate(180, Rotate.Y_AXIS));
 
 
+        ImageView imageView = new ImageView(new Image("main/res/Card_Images/cardBack.png"));
+        imageView.setFitWidth(50);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+        imageView.fitWidthProperty().bind(inside_card2.widthProperty());
 
-        switch (card) {
-            case "SpadesAce":
-                imageView2.setImage(image);
-                break;
-            case "SpadesKing":
-                imageView2.setImage(image1);
-                break;
-            case "SpadesQueen":
-                imageView2.setImage(image2);
-                break;
-            case "SpadesJack":
-                imageView2.setImage(image3);
-                break;
-            case "SpadesTen":
-                imageView2.setImage(image4);
-                break;
-            case "SpadesNine":
-                imageView2.setImage(image5);
-                break;
-            case "SpadesEight":
-                imageView2.setImage(image6);
-                break;
-            case "SpadesSeven":
-                imageView2.setImage(image7);
-                break;
-            case "SpadesSix":
-                imageView2.setImage(image8);
-                break;
-            case "SpadesFive":
-                imageView2.setImage(image9);
-                break;
-            case "SpadesFour":
-                imageView2.setImage(image10);
-                break;
-            case "SpadesThree":
-                imageView2.setImage(image11);
-                break;
-            case "SpadesTwo":
-                imageView2.setImage(image12);
-                break;
-            case "HeartsAce":
-                imageView2.setImage(image13);
-                break;
-            case "HeartsKing":
-                imageView2.setImage(image14);
-                break;
-            case "HeartsQueen":
-                imageView2.setImage(image15);
-                break;
-            case "HeartsJack":
-                imageView2.setImage(image16);
-                break;
-            case "HeartsTen":
-                imageView2.setImage(image17);
-                break;
-            case "HeartsNine":
-                imageView2.setImage(image18);
-                break;
-            case "HeartsEight":
-                imageView2.setImage(image19);
-                break;
-            case "HeartsSeven":
-                imageView2.setImage(image20);
-                break;
-            case "HeartsSix":
-                imageView2.setImage(image21);
-                break;
-            case "HeartsFive":
-                imageView2.setImage(image22);
-                break;
-            case "HeartsFour":
-                imageView2.setImage(image23);
-                break;
-            case "HeartsThree":
-                imageView2.setImage(image24);
-                break;
-            case "HeartsTwo":
-                imageView2.setImage(image25);
-                break;
-            case "DiamondsAce":
-                imageView2.setImage(image26);
-                break;
-            case "DiamondsKing":
-                imageView2.setImage(image27);
-                break;
-            case "DiamondsQueen":
-                imageView2.setImage(image28);
-                break;
-            case "DiamondsJack":
-                imageView2.setImage(image29);
-                break;
-            case "DiamondsTen":
-                imageView2.setImage(image30);
-                break;
-            case "DiamondsNine":
-                imageView2.setImage(image31);
-                break;
-            case "DiamondsEight":
-                imageView2.setImage(image32);
-                break;
-            case "DiamondsSeven":
-                imageView2.setImage(image33);
-                break;
-            case "DiamondsSix":
-                imageView2.setImage(image34);
-                break;
-            case "DiamondsFive":
-                imageView2.setImage(image35);
-                break;
-            case "DiamondsFour":
-                imageView2.setImage(image36);
-                break;
-            case "DiamondsThree":
-                imageView2.setImage(image37);
-                break;
-            case "DiamondsTwo":
-                imageView2.setImage(image38);
-                break;
-            case "ClubsAce":
-                imageView2.setImage(image39);
-                break;
-            case "ClubsKing":
-                imageView2.setImage(image40);
-                break;
-            case "ClubsQueen":
-                imageView2.setImage(image41);
-                break;
-            case "ClubsJack":
-                imageView2.setImage(image42);
-                break;
-            case "ClubsTen":
-                imageView2.setImage(image43);
-                break;
-            case "ClubsNine":
-                imageView2.setImage(image44);
-                break;
-            case "ClubsEight":
-                imageView2.setImage(image45);
-                break;
-            case "ClubsSeven":
-                imageView2.setImage(image46);
-                break;
-            case "ClubsSix":
-                imageView2.setImage(image47);
-                break;
-            case "ClubsFive":
-                imageView2.setImage(image48);
-                break;
-            case "ClubsFour":
-                imageView2.setImage(image49);
-                break;
-            case "ClubsThree":
-                imageView2.setImage(image50);
-                break;
-            case "ClubsTwo":
-                imageView2.setImage(image51);
-                break;
-
-        }
-*/
-
-        //Text Rank = new Text(c.getRank().toString());
-        //Text Suit = new Text(c.getSuit().toString());
 
         inside_card2.getChildren().add(imageView2);
+        inside_card2.getChildren().add(imageView);
+
         inside_card2.setPrefSize(100, 200);
         inside_card2.setMaxSize(100, 200);
 
         inside_card2.setStyle("-fx-background-color: #FFFFFF");
+        container.setMaxSize(100, 200);
+        container.getChildren().add(inside_card2);
 
 
-        return inside_card2;
+        return container;
     }
 
     public Pane Empty_Card_Apperance(Card c) {
