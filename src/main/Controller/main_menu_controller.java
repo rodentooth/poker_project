1b7dbb8317e283062edac1cc1e9a7b9363b00dc7
Joel Marugg <joel.marugg@students.fhnw.ch>
2018-03-15 11:49:18 +0100

    Main menu umstrukturiert

--- src/main/Controller/main_menu_controller.java
+++ src/main/Controller/main_menu_controller.java
@@ -41,15 +41,17 @@ public class main_menu_controller {
                 view.node3.getChildren().add(playerName);
             }
 
-            view.play.setDisable(false);
-            if (view.play.isDisable() == false) {
-                view.node4.getChildren().remove(1);
+            view.offlineBtn.setDisable(false);
+
+            view.onlineBtn.setDisable(false);
+            if (view.onlineBtn.isDisable() == false) {
+                view.offlineBtn_box.getChildren().remove(1);
             }
 
         });
 
 
-        view.play.setOnAction((event) -> {
+        view.onlineBtn.setOnAction((event) -> {
 
 
             for (int i = 0; i < number_of_players; i++) {
@@ -84,6 +86,7 @@ public class main_menu_controller {
             /*            poker_game_1_controller game_1_controller = new poker_game_1_controller(model, gameView, savedNames);
              */
 
+
             Online_Poker_5_Stud model2 = new Online_Poker_5_Stud();
             online_poker_game_1_controller game_2_controller = new online_poker_game_1_controller(model2, gameView, savedNames);
 
@@ -91,6 +94,51 @@ public class main_menu_controller {
 
 
         });
+
+        ///////////////////////////////////////offline modus
+        view.offlineBtn.setOnAction((event) -> {
+
+
+            for (int i = 0; i < number_of_players; i++) {
+
+
+                if (!(((TextField) (view.node3.getChildren().get(i))).getText().isEmpty())) {
+                    savedNames.add(((TextField) (view.node3.getChildren().get(i))).getText());
+                    System.out.println(savedNames.get(i));
+                } else {
+                    ((TextField) (view.node3.getChildren().get(i))).setText("Player " + (i + 1));
+                    savedNames.add(((TextField) (view.node3.getChildren().get(i))).getText());
+                    System.out.println(savedNames.get(i));
+
+                }
+            }
+
+
+            System.out.println("You clicked me!");
+            view.deck_txt.setText("That's the main menu");
+
+
+            //Here I want to swap the screen!
+
+            Stage stageTheEventSourceNodeBelongs = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            // OR
+            Stage stageTheLabelBelongs = (Stage) view.deck_txt.getScene().getWindow();
+            // these two of them return the same stage
+            // Swap screen::
+
+            poker_game_1 gameView = new poker_game_1(stageTheEventSourceNodeBelongs);
+
+            poker_game_1_controller game_1_controller = new poker_game_1_controller(model, gameView, savedNames);
+
+
+            //Online_Poker_5_Stud model2 = new Online_Poker_5_Stud();
+            //online_poker_game_1_controller game_2_controller = new online_poker_game_1_controller(model2, gameView, savedNames);
+
+            //stageTheLabelBelongs.setScene(new Scene(new Pane()));
+
+
+        });
+        /////////////////////////////////////////////finito///////////////
 
 /*
         // register ourselves to handle window-closing event
--- src/main/View/main_menu.java
+++ src/main/View/main_menu.java
@@ -2,35 +2,57 @@ package main.View;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-
 
 
 public class main_menu {
 
-	public Button play;
+    public Button onlineBtn;
 	public Label deck_txt;
     public ComboBox playerDropdown;
     public HBox node3;
     public HBox node4;
     public boolean playerNumberEntered = false;
+    public Button offlineBtn;
+    public HBox offlineBtn_box;
 
     public main_menu(Stage primaryStage) {
 
 
         //TODO Knopf für online game  UND NÄÄÄme ich wirde schwarz ma
 
+        //Offline/Online
+        final ToggleGroup group = new ToggleGroup();
+
+
+        ToggleButton tb1 = new ToggleButton("Online");
+        tb1.setToggleGroup(group);
+        tb1.setSelected(true);
+
+        ToggleButton tb2 = new ToggleButton("Offline");
+        tb2.setToggleGroup(group);
+
+        HBox toggleBox = new HBox();
+        toggleBox.getChildren().addAll(tb1, tb2);
+        toggleBox.setAlignment(Pos.CENTER);
+
 
         //Titel
-        HBox container_title = new HBox();
+        VBox container_title = new VBox();
         deck_txt = new Label("Poker 5 Stud");
         container_title.getChildren().add(deck_txt);
+        container_title.getChildren().add(toggleBox);
+        container_title.setAlignment(Pos.CENTER);
+
+
+
 
         //Number of Players
         HBox container_game_setup = new HBox();
@@ -51,47 +73,69 @@ public class main_menu {
         //playerDropdown.getSelectionModel().selectFirst();
         container_game_setup.getChildren().add(lbl2);
         container_game_setup.getChildren().add(playerDropdown);
+        container_game_setup.setAlignment(Pos.CENTER);
+
+
 
         //Names
         HBox node2 = new HBox();
         Label lbl3 = new Label("Names: ");
+        lbl3.setAlignment(Pos.CENTER);
         node2.getChildren().add(lbl3);
+        node2.setAlignment(Pos.CENTER);
+
 
         //Textfields for names
         node3 = new HBox();
+        node3.setAlignment(Pos.CENTER);
 
 
-
-        //Button to Play
-        node4 = new HBox();
-        play = new Button("Play Online!");
-        Button offlineBtn = new Button("Play Offline");
+        //Button to play offline
+        offlineBtn_box = new HBox();
+        offlineBtn = new Button("Play Offline");
+        offlineBtn.setDisable(true);
         Label enterPlayerNumber = new Label("Please, enter number of players to continue.");
-        play.setDisable(true);
-        node4.getChildren().add(play);
-        node4.getChildren().add(offlineBtn);
-        node4.getChildren().add(enterPlayerNumber);
+        offlineBtn_box.getChildren().addAll(offlineBtn, enterPlayerNumber);
+        offlineBtn_box.setAlignment(Pos.CENTER);
 
 
+        //Button to Play online
+        onlineBtn = new Button("Play Online!");
+        onlineBtn.setDisable(true);
+        onlineBtn.setAlignment(Pos.CENTER);
 
 
+        HBox centerBox = new HBox();
 
 
+        VBox rightBox = new VBox();
+        rightBox.setSpacing(25);
+        rightBox.getChildren().addAll(container_game_setup, node2, node3, offlineBtn_box);
+        rightBox.setAlignment(Pos.CENTER);
+        rightBox.setStyle("-fx-background-color: #fffa00");
+        rightBox.prefWidthProperty().bind(centerBox.widthProperty());
 
 
-
-        /*
-         * todo VARIABLENÄME KOLLEG was isch do nid guet? das sind doch alles nodes vode vbox oder?^^
-         */
-        VBox root = new VBox();
-        root.getChildren().add(container_title);
-        root.getChildren().add(container_game_setup);
-        root.getChildren().add(node2);
-        root.getChildren().add(node3);
-        root.getChildren().add(node4);
+        HBox leftBox = new HBox();
+        leftBox.getChildren().addAll(onlineBtn);
+        leftBox.setAlignment(Pos.CENTER);
+        leftBox.prefWidthProperty().bind(centerBox.widthProperty());
 
 
-        Scene scene = new Scene(root, 700, 400);
+        Line middleLine = new Line();
+        middleLine.setStartY(100);
+        middleLine.setEndY(400);
+
+        centerBox.getChildren().addAll(leftBox, middleLine, rightBox);
+        centerBox.setAlignment(Pos.CENTER);
+
+        BorderPane root = new BorderPane();
+        root.setTop(container_title);
+        root.setCenter(centerBox);
+        //root.setAlignment(centerBox,Pos.CENTER);
+
+
+        Scene scene = new Scene(root, 1200, 700);
         scene.getStylesheets().add(
                 getClass().getResource("poker.css").toExternalForm());
         primaryStage.setTitle("Hello World");
